c770e45b61c45a44aed9a57da6503f06217fbcab
Anas Ahmed <officialanasahmed2001@gmail.com>
2026-05-24 17:43:09 +0530

    Initial setup DearDjango Journal

---
+++ DearDjango/urls.py
@@ -0,0 +1,9 @@
+"""DearDjango URL Configuration"""
+from django.contrib import admin
+from django.urls import path , include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(('learning_logs.urls', 'learning_logs'),
+                    namespace='learning_logs')),
+]
---
+++ learning_logs/urls.py
@@ -0,0 +1,9 @@
+"""define URL patterns for learning_logs"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+]
